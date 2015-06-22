d8cf293dc26b6c4339e6a0bc55a412a1e0e8ca9c
Jefferson Stachelski <jefferson.stachelski@gmail.com>
2015-06-22 19:21:33 -0300

    Flags parder added

--- src/pixelated/main/main.go
+++ src/pixelated/main/main.go
@@ -2,10 +2,25 @@ package main
 
 import (
   "fmt"
+  "flag"
   "pixelated/auth"
 )
 
+func initArgs() (string, string, string, string){
+  var user, password, provider, cert string
+
+  flag.StringVar(&user, "user", "", "User name")
+  flag.StringVar(&password, "password", "", "Password")
+  flag.StringVar(&provider, "provider", "", "Server provider")
+  flag.StringVar(&cert, "cert", "", "Provider certificate")
+  flag.Parse()
+
+  return user, password, provider, cert
+}
+
 func main() {
+  user, password, provider, cert := initArgs()
+
   serverInfo := auth.ServerInfo{"ha", false, 1}
   credentials := auth.Authenticate("username", "password", serverInfo)
 
