85fe983b6607aaf9afec7d8045c548fae93e58c2
Victor Shyba <victor.shyba@gmail.com>
2015-07-02 18:12:51 +0200

    changing project structure to reduce namespace length

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"./auth"
+	"flag"
+	"fmt"
+)
+
+func initArgs() (string, string, string, string) {
+	var user, password, provider, cert string
+	var skipCertValidation bool
+
+	flag.StringVar(&user, "user", "", "User name")
+	flag.StringVar(&password, "password", "", "Password")
+	flag.StringVar(&provider, "provider", "", "Server provider")
+	flag.StringVar(&cert, "cert", "", "Provider certificate")
+	flag.BoolVar(&skipCertValidation, "insecure", false, "Skip certificate validation")
+	flag.Parse()
+
+	return user, password, provider, cert
+}
+
+func main() {
+	user, password, provider, cert := initArgs()
+
+	serverInfo := auth.ServerInfo{provider, cert, 1}
+	credentials := auth.Authenticate(user, password, serverInfo)
+
+	fmt.Println(credentials.SessionID)
+	fmt.Println(credentials.UserToken)
+	fmt.Println(credentials.Username)
+	fmt.Println(credentials.UUID)
+
+}
